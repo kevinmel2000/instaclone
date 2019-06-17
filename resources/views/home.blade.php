9e3c8bf69889e3c249d3b09376155c22430ed150
tokanggit <tokang.dev@gmail.com>
2019-06-17 23:52:09 +0700

    edit home layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,34 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
+  <div class="row">
+    <div class="col-3 p-5">
+      <img src="https://scontent-sin2-2.cdninstagram.com/vp/c88f0cc97185aa6d9a1ff7ce69b00c45/5D86693E/t51.2885-19/s150x150/49622765_358871694905259_1855250538422075392_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=103"
+      class="rounded-circle">
     </div>
+    <div class="col-9 pt-5">
+      <div><h1>Instaclone</h1></div>
+      <div class="d-flex">
+        <div class="pr-5"><strong>153</strong> posts</div>
+        <div class="pr-5"><strong>23k</strong> followers</div>
+        <div class="pr-5"><strong>212</strong> following</div>
+      </div>
+      <div class="pt-4 font-weight-bold">Instaclone</div>
+      <div>❤️ Allah , ❤️ Istri</div>
+      <div><a href="#">www.instaclone.com</a></div>
+    </div>
+  </div>
+
+  <div class="row pt-5">
+    <div class="col-4">
+      <img src="https://scontent-sin2-2.cdninstagram.com/vp/e64ddcac2ec0d12519d006cc7b422c9e/5D8B074D/t51.2885-15/e35/s320x320/59867862_295870571362661_513422285186217641_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=105" alt="" class="w-100">
+    </div>
+    <div class="col-4">
+      <img src="https://scontent-sin2-2.cdninstagram.com/vp/79b1829493778fa31e7b636fe221c727/5D95AFB2/t51.2885-15/e35/c135.0.809.809a/s240x240/44819200_570203383414626_693827797858661662_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=106" alt="" class="w-100">
+    </div>
+    <div class="col-4">
+      <img src="https://scontent-sin2-2.cdninstagram.com/vp/79176476e81416d403b769cedb6a0094/5DC2B8D7/t51.2885-15/e35/c90.0.900.900/s240x240/43145944_311295909690883_8267335133361871379_n.jpg?_nc_ht=scontent-sin2-2.cdninstagram.com&_nc_cat=107" alt="" class="w-100">
+    </div>
+  </div>
 </div>
 @endsection
