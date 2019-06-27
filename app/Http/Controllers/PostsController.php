970b69b44c71c48b88335fc1791121dd97e00d70
tokanggit <tokang.dev@gmail.com>
2019-06-27 22:31:03 +0700

    add home screen

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Intervention\Image\Facades\Image;
 
@@ -9,6 +10,14 @@ class PostsController extends Controller
 {
     public function __construct() {
       $this->middleware('auth');
+    }
+
+    public function index() {
+      $users = auth()->user()->following()->pluck('profiles.user_id');
+
+      $posts = Post::whereIn('user_id', $users)->latest()->get();
+      
+      return view('posts.index', compact('posts'));
     }
 
     public function create() {
