528f96d1b98c2aa317534c3c3d394ea8d73ed66c
tokanggit <tokang.dev@gmail.com>
2019-06-27 23:23:07 +0700

    add mail service

--- app/User.php
+++ app/User.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -44,6 +46,8 @@ class User extends Authenticatable
         $user->profile()->create([
           'title' => $user->username
         ]);
+
+        Mail::to($user->email)->send(new NewUserWelcomeMail());
       });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Mail\NewUserWelcomeMail;
+
 
 Auth::routes();
+
+Route::get('/email', function() {
+  return new NewUserWelcomeMail();
+});
 
 Route::post('/follow/{user}', 'FollowsController@store');
 
